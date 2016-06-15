6728b9ffa54ac9aa8667081037a0822b9afff075
LraiZer <dev@ukcvs.net>
2016-06-15 20:26:42 +0100

    add missing declaration for delete event_id

--- src/common/epgdb/epgdb_titles.h
+++ src/common/epgdb/epgdb_titles.h
@@ -7,5 +7,6 @@ int epgdb_calculate_mjd (time_t value);
 int epgdb_titles_count (epgdb_channel_t *channel);
 epgdb_title_t *epgdb_titles_get_by_id_and_mjd (epgdb_channel_t *channel, unsigned short int event_id, unsigned short int mjd_time);
 epgdb_title_t *epgdb_titles_add (epgdb_channel_t *channel, epgdb_title_t *title);
+void epgdb_titles_delete_event_id (epgdb_channel_t *channel, unsigned short int event_id);
 
 #endif // _EPGDB_TITLES_H_
