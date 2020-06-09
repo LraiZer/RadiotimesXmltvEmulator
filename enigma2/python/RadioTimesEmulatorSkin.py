8fc36b796dc14a6e4e11cf187efd48a9b57f76e7
LraiZer <lbound@hotmail.com>
2020-06-09 14:54:48 +0100

    update PEP8 - W291 trailing whitespace

--- enigma2/python/RadioTimesEmulatorSkin.py
+++ enigma2/python/RadioTimesEmulatorSkin.py
@@ -5,8 +5,8 @@ from __future__ import division
 # There are seven skinable outputs as follows:
 #
 # 1) self["Frontend"] > FrontendStatus
-# 2) self["action"] > Label, 
-# 3) self["actionLong"] > Label, 
+# 2) self["action"] > Label,
+# 3) self["actionLong"] > Label,
 # 4) self["status"] > Label,
 # 5) self["tuner_text"] > Label, This is the tuner letter
 # 6) self["progress"] > ProgressBar
